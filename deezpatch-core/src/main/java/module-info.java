484880be396a5801e2c41155e4f93db32391d29e
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-04-28 15:45:25 -0700

    Add module-info.java

---
+++ deezpatch-core/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/** Deezpatch core components. */
+module io.github.joeljeremy.deezpatch.core {
+  exports io.github.joeljeremy.deezpatch.core;
+  exports io.github.joeljeremy.deezpatch.core.invocationstrategies;
+
+  requires static org.checkerframework.checker.qual;
+}
