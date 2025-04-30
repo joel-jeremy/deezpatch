793b0543954eddb688151a41dfef3500abb5cb21
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-04-30 13:15:36 -0700

    Exclude module-info.java and package-info.java from Eclipse classpath

--- build-logic/src/main/kotlin/deezpatch.eclipse-conventions.gradle.kts
+++ build-logic/src/main/kotlin/deezpatch.eclipse-conventions.gradle.kts
@@ -1,15 +1,35 @@
 import org.gradle.plugins.ide.eclipse.model.AbstractClasspathEntry
 import org.gradle.plugins.ide.eclipse.model.Classpath
+import org.gradle.plugins.ide.eclipse.model.SourceFolder
 
 plugins {
   id("eclipse")
 }
 
-eclipse.classpath.file.whenMerged(Action<Classpath> {
-  // To fix an issue in Eclipse buildship where dependent projects cannot resolve classes
-  // of the dependee project if dependee has JMH sources.
-  entries
-      .filterIsInstance<AbstractClasspathEntry>()
-      .filter { it.path.startsWith("src/jmh") }
-      .forEach { it.entryAttributes["test"] = "true" }
-})
+eclipse {
+  classpath.file.whenMerged(Action<Classpath> {
+    // To fix an issue in Eclipse buildship where dependent projects cannot resolve classes
+    // of the dependee project if dependee has JMH sources.
+    entries
+        .filterIsInstance<AbstractClasspathEntry>()
+        .filter { it.path.startsWith("src/jmh") }
+        .forEach { it.entryAttributes["test"] = "true" }
+
+    excludeInfoFiles(this)
+  })
+}
+
+/**
+ * Exclude module-info and package-info when compiling through Eclipse.
+ * @see https://github.com/ben-manes/caffeine/blob/master/gradle/plugins/src/main/kotlin/lifecycle/eclipse.caffeine.gradle.kts
+ */
+fun excludeInfoFiles(classpath: Classpath) {
+  classpath.entries.filterIsInstance<SourceFolder>().forEach {
+    val excludes = it.excludes.toMutableList()
+    excludes += "module-info.java"
+    if (it.path != "src/main/java") {
+      excludes += "**/package-info.java"
+    }
+    it.excludes = excludes
+  }
+}
