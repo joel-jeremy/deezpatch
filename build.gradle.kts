304752f6f49f3d46d3c8496a3a9fd999dc6a1a23
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2026-01-14 11:51:16 -0800

    Remove coveralls (#211)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
   id("emissary.nexus-publish-conventions")
   id("emissary.java-reporting-conventions")
   id("emissary.sonar-conventions")
-  id("emissary.coveralls-conventions")
   id("emissary.snyk-conventions")
   id("emissary.dependency-updates-conventions")
   id("emissary.eclipse-conventions")
